31520b0d234e6362cfa5e061f1a8946fafb09b24
DESKTOP-ES461D3\Rafael <Rafaelaleman1988@gmail.com>
2018-07-04 18:05:40 +0200

    Validacion de certificados autofirmados y revocados

--- TFG/app/src/main/java/com/example/rafa/tfg/rest/RestImpl.java
+++ TFG/app/src/main/java/com/example/rafa/tfg/rest/RestImpl.java
@@ -3,6 +3,16 @@ package com.example.rafa.tfg.rest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -13,12 +23,61 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RestImpl {
 
     public static RestInterface getRestInstance(){
+        OkHttpClient okHttpClient = getUnsafeOkHttpClient();
         Gson gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
                 .create();
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://192.168.1.104:8000").addConverterFactory(GsonConverterFactory.create(gson))
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://192.168.1.104:4433")
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
         RestInterface rest = retrofit.create(RestInterface.class);
         return rest;
     }
+
+
+        public static OkHttpClient getUnsafeOkHttpClient() {
+            try {
+                // Create a trust manager that does not validate certificate chains
+                final TrustManager[] trustAllCerts = new TrustManager[] {
+                        new X509TrustManager() {
+                            @Override
+                            public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                            }
+
+                            @Override
+                            public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                            }
+
+                            @Override
+                            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                                return new java.security.cert.X509Certificate[]{};
+                            }
+                        }
+                };
+
+                // Install the all-trusting trust manager
+                final SSLContext sslContext = SSLContext.getInstance("SSL");
+                sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+                // Create an ssl socket factory with our all-trusting manager
+                final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+                OkHttpClient.Builder builder = new OkHttpClient.Builder();
+                builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
+                builder.hostnameVerifier(new HostnameVerifier() {
+                    @Override
+                    public boolean verify(String hostname, SSLSession session) {
+                        return true;
+                    }
+                });
+
+                OkHttpClient okHttpClient = builder.build();
+                return okHttpClient;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
 }
